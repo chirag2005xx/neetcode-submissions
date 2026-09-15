425cde32fb145aa49eced7dac25c5cb60cf4afc3
chirag2005xx <137156223+chirag2005xx@users.noreply.github.com>
2026-09-15 23:43:39 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int i=0;
+        int j=1;
+        int maxdiff=0;
+        while(j<prices.size()){
+            if(prices[j]-prices[i]<0){
+                i=j;
+                j++;
+            }
+            else{
+
+                int diff=prices[j]-prices[i];
+
+                maxdiff=max(maxdiff,diff);
+                j++;
+
+
+            }
+        }
+
+        return maxdiff;
+        
+    }
+};
